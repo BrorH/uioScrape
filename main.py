97cbb30cf25d98f68003ed17aadc66f3dea831d8
BrorH <bahjemga@uio.no>
2021-01-28 13:37:33 +0100

    rescructured program into classes. Now much easier to work with

--- main.py
+++ main.py
@@ -4,27 +4,98 @@ from html.parser import HTMLParser
 import urllib.request
 import re,sys, requests
 
-global url, visited, parent_url, rec, subject_type, subject_uuid
-visited = []
-pdfs = {}
-pdfnames = []
-valid_pdfs = []
-potential_pdfs = []
-rec = 0
+
 
 ignore_pdfs = ["lecture", "smittevern", "Lecture", "oblig", "week", "Week", "exercise", "Oblig", "ukesoppgave", "Ukesoppgave", "oppgave", "Oppgave"]
 
-class LinkScrapeIndex(HTMLParser):
 
+class LinkScrape:
+    urls = []
+    parent_urls = []
+    visited = []
+    pdfs = {}
+    valid_pdfs = []
+    potential_pdfs = []
+    subject_subfaculty_dict = {"fys":"fys", "fys-mek":"fys", "in":"ifi", "mat":"math", "mek":"math", "ast":"astro"}
+    def __init__(self, subject_code):
+        
+
+        self.base_url = "https://www.uio.no/studier/emner/matnat/"
+        self.subject_code = subject_code.upper()
+        subject_regex = re.search(r"([a-zA-Z\-]+)(\d+)", self.subject_code)
+        if subject_regex:
+            self.subject_type = subject_regex.group(1)
+            self.subject_uuid = subject_regex.group(2)
+        else:
+            print(f"Error, subject code '{subject_code}' is note valid")
+            sys.exit(1)
+        try:
+            self.subfaculty = self.subject_subfaculty_dict[self.subject_type.lower()]
+        except KeyError:
+            print(f"Subject '{subject_code}' of type '{self.subject_type}' not yet supported")
+            sys.exit(1)
+        
+    
+    def start(self, **kwargs):
+        self.start_index_scraper()
+        try:
+            for i,parent_url in enumerate(self.parent_urls):
+                
+                #print(f"\nchecking {index_url} ... \n")
+                try:
+                    self.child_scraping(parent_url)
+                except urllib.error.HTTPError:
+                    pass
+                print(f"{i+1}/{len(self.parent_urls)}")
+        except KeyboardInterrupt:
+            pass
+
+        
+
+    
+
+    def start_index_scraper(self):
+
+        url = f"https://www.uio.no/studier/emner/matnat/{self.subfaculty}/{self.subject_code}/"
+        self.url = url
+        self.parent_urls.append(url)
+        self.visited.append(url)
+        request_object = urllib.request.Request(url)
+        page_object = urllib.request.urlopen(request_object)
+        link_parser = LinkScrapeStartIndex()
+        link_parser.parentScrape = self
+        link_parser.feed(page_object.read().decode("latin-1"))
+        
+        self.parent_urls = link_parser.urls[:]
+    
+    def child_scraping(self, url, depth=0):
+        
+        self.visited.append(url) 
+
+        request_object = urllib.request.Request(url)
+        page_object = urllib.request.urlopen(request_object)
+        link_parser = LinkScrapeChild()
+        link_parser.parentScrape = self 
+        link_parser.parent_url = url
+        link_parser.pdfs = self.pdfs 
+        link_parser.feed(page_object.read().decode("latin-1"))
+
+        if depth <= 1: 
+            for url__ in link_parser.urls:
+                self.child_scraping(url__, depth + 1)
+        return link_parser.urls
+
+class LinkScrapeStartIndex(HTMLParser):
+    urls = []
+    
     def handle_starttag(self, tag, attrs):
-        #print(tag, attrs)
         if tag == 'a':
             for attr in attrs:
                 if attr[0] == 'href':
                     link = attr[1]
                     if "@" in link: continue
-                    if link == url: continue
-                    if not link.startswith(url): continue
+                    if link == self.parentScrape.url: continue
+                    if not link.startswith(self.parentScrape.url): continue
                     if link.endswith("/index-eng.html"): continue
                     
 
@@ -34,8 +105,11 @@ class LinkScrapeIndex(HTMLParser):
                     if link.find('http') >= 0:
                         if link not in self.urls:
                             self.urls.append(link)
+        
+
 
 class LinkScrapeChild(HTMLParser):
+    urls = []
 
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
@@ -54,22 +128,21 @@ class LinkScrapeChild(HTMLParser):
                                     break 
                             if ignored: continue
                             try:
-                                if (pdfname not in list(pdfs.keys()) ) or (len(link) > len(pdfs[pdfname])):
-                                    pdfs[pdfname] = fill_incomplete_url(link)
+                                if (pdfname not in list(self.pdfs.keys()) ) or (len(link) > len(self.pdfs[pdfname])):
+                                    self.pdfs[pdfname] = fill_incomplete_url(link, self.parent_url)
                                     name = pdfname.lower()
-                                    #print(fill_incomplete_url(link), "aaa")
                                     try:
                                         if "ex" in name or "exam" in name or "eksamen" in name or "eks" in name:
                                             timeout = 10
                                         else:
                                             timeout = 1
                                         with eventlet.Timeout(timeout):
-                                            if requests.get(pdfs[pdfname]).status_code == 200:
-                                                print(pdfname, pdfs[pdfname])
-                                                valid_pdfs.append(pdfs[pdfname])
+                                            if requests.get(self.pdfs[pdfname]).status_code == 200:
+                                                print(pdfname, self.pdfs[pdfname])
+                                                self.parentScrape.valid_pdfs.append(self.pdfs[pdfname])
                                     except eventlet.timeout.Timeout:
-                                        potential_pdfs.append(pdfs[pdfname])
-                                        print("Timed out:", pdfname, pdfs[pdfname])
+                                        self.parentScrape.potential_pdfs.append(self.pdfs[pdfname])
+                                        print("Timed out:", pdfname, self.pdfs[pdfname])
 
                             except KeyError:
                                 continue
@@ -79,16 +152,18 @@ class LinkScrapeChild(HTMLParser):
                     
                     if link.find('http') >= 0:
                         if re.search(r".*\?[^/]+",link): continue
-                        if not parent_url in link: continue
-                        if link in visited: continue
+                        if not self.parent_url in link: continue
+                        if link in self.parentScrape.visited: continue
                         if "@" in link: continue
-                        if link == parent_url: continue
-                        if link == url: continue
+                        if link in self.parentScrape.parent_urls: continue
+                        if link == self.parentScrape.url: continue
                         if link not in self.urls:
                             self.urls.append(link)
 
 
-def fill_incomplete_url(link):
+
+
+def fill_incomplete_url(link, parent_url):
     if link.startswith('http'):
         return link
     # add check for correct top level domain
@@ -105,86 +180,14 @@ def fill_incomplete_url(link):
             return parent_url +"/" + link
     
 
-
-def start_index_scraper():
-    global url, subject_code, parent_url, visited, subfaculty, subject_type
-    #subject_code = subject.upper()
-
-    url = f"https://www.uio.no/studier/emner/matnat/{subfaculty}/{subject_code}/"
-    parent_url = url
-    visited.append(url)
-    request_object = urllib.request.Request(url)
-    page_object = urllib.request.urlopen(request_object)
-    link_parser = LinkScrapeIndex()
-    link_parser.urls = []
-    link_parser.feed(page_object.read().decode("latin-1"))
-
-    return link_parser.urls 
-
-def child_scraping(url_, depth = 0):
-    global url,visited, rec
-    url = url_
-    visited.append(url) 
-
-    request_object = urllib.request.Request(url)
-    page_object = urllib.request.urlopen(request_object)
-    link_parser = LinkScrapeChild()
-    link_parser.urls = []
-    link_parser.feed(page_object.read().decode("latin-1"))
-   
-    if depth <= 1:
-        for url__ in link_parser.urls:
-            rec += 1
-            child_scraping(url__, depth + 1)
-    return link_parser.urls
-
-
-deep = 3
-
-def initializer(subject):
-    global subject_type, subject_uuid, subject_code, subfaculty
-    # extracts subject code, subject type (mat, fys etc) and does other initializing
-
-    subject_subfaculty_dict = {"fys":"fys", "fys-mek":"fys", "in":"ifi", "mat":"math", "mek":"math", "ast":"astro"}
-
-    subject_code = subject.upper()
-    subject_regex = re.search(r"([a-zA-Z\-]+)(\d+)", subject)
-    if subject_regex:
-        subject_type = subject_regex.group(1)
-        subject_uuid = subject_regex.group(2)
-    else:
-        print(f"Error, subject code '{subject}' is note valid")
-        sys.exit(1)
-    try:
-        subfaculty = subject_subfaculty_dict[subject_type]
-    except KeyError:
-        print(f"Subject '{subject_code}' of type '{subject_type}' not yet supported")
-        sys.exit(1)
-    
-        
-
-
 if __name__ == '__main__':
-    try:
-        initializer(sys.argv[1])
-        index_urls = start_index_scraper()
-        print(f"Found {len(index_urls)} links of interest on main semester page")
+    scraper = LinkScrape(sys.argv[1])
+    scraper.start()
 
-        for i,index_url in enumerate(index_urls):
-            
-            #print(f"\nchecking {index_url} ... \n")
-            parent_url = index_url
-            try:
-                child_urls = child_scraping(index_url)
-            except urllib.error.HTTPError:
-                pass
-            print(f"{round(((i+1)*100)//len(index_urls))}%")
-    except KeyboardInterrupt:
-        pass
     print()
     print("===RESULTS===")
-    print("\n".join(valid_pdfs))
+    print("\n".join(scraper.valid_pdfs))
 
     print()
     print("===POTENTIAL PDFS (TIMED OUT)===")
-    print("\n".join(potential_pdfs))
+    print("\n".join(scraper.potential_pdfs))

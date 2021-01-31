24431fc2f50a7b1df9241406f8c844c85aee8c3b
BrorH <bahjemga@uio.no>
2021-02-01 00:55:10 +0100

    made code faster, addded arg parsing and commented code. now officially in ver. 1.0

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from html.parser import HTMLParser
-import re, sys, requests, threading, itertools
+import re, sys, requests, threading, itertools, time, argparse
 from urllib import request
 import urllib.error
 from multiprocessing import Queue
@@ -18,8 +18,93 @@ with open("priorities.txt", "r") as file:
         priorities[words[0]] = int(words[-1])
 # list of strings which will automaticall disqualify a pdf from being stored
 ignore_pdfs = ["devilry","lecture", "smittevern", "Lecture", "oblig", "week", "Week", "exercise", "Oblig", "ukesoppgave", "Ukesoppgave", "oppgave", "Oppgave"]
-max_requests = 1000 # dont make more than 1000 requests to the uio servers
 num_requests = 0
+
+class LinkScrape:
+    urls = []
+    parent_urls = []
+    visited = []
+    pdfs = {}
+    valid_pdfs = []
+    potential_pdfs = []
+
+    subject_subfaculty_dict = {"fys":"fys", "fys-mek":"fys", "in":"ifi", "mat":"math", "mek":"math", "ast":"astro", "kjm":"kjemi", "bios":"ibv", "bios-in":"ibv", "farm":"farmasi", "fys-stk":"fys"}
+    def __init__(self, subject, max_depth, max_requests):
+        self.max_depth = max_depth
+        self.max_requests = max_requests
+        
+
+        self.base_url = "https://www.uio.no/studier/emner/matnat/"
+        self.subject_code = subject.upper()
+        subject_regex = re.search(r"([a-zA-Z\-]+)(\d+)", self.subject_code)
+        if subject_regex:
+            self.subject_type = subject_regex.group(1)
+            self.subject_uuid = subject_regex.group(2)
+        else:
+            print(f"Error, subject code '{subject}' is note valid")
+            sys.exit(1)
+        try:
+            self.subfaculty = self.subject_subfaculty_dict[self.subject_type.lower()]
+        except KeyError:
+            print(f"Subject '{subject}' of type '{self.subject_type}' not yet supported")
+            sys.exit(1)
+        
+    
+    def start(self, depth=0,**kwargs):
+        print(f"Starting scrape of {self.subject_code   } of max depth {self.max_depth} and max requests {self.max_requests}")
+        self.start_index_scraper()
+        self.urls_to_be_checked = self.parent_urls.copy()  # urls_to_be_checked gets dynamically updated
+        try:
+            for i in range(self.max_depth):
+                
+                child_urls_ = fetch_parallel(self.urls_to_be_checked)#, iteration=i)
+                child_urls = [foo[0][1:] for foo in child_urls_]
+                #child_urls = list(itertools.chain.from_iterable(child_urls))
+                
+                if i == 0:
+                    parent_urls = [foo[0][0] for foo in child_urls_]
+                new_urls_to_be_checked = []
+                for childs in child_urls:
+                    for url in childs[1:]:
+                        if scrape_result:=check_url_and_update_storage(self, url, childs[0], parent_urls):
+                            new_urls_to_be_checked.append(scrape_result)
+                        
+                
+                self.urls_to_be_checked = reorder_urls_by_priority(new_urls_to_be_checked.copy())
+                #print(self.urls_to_be_checked)
+        except KeyboardInterrupt:
+            pass
+
+        
+
+    
+
+    def start_index_scraper(self):
+
+        url = f"https://www.uio.no/studier/emner/matnat/{self.subfaculty}/{self.subject_code}/"
+        self.url = url
+        self.parent_urls.append(url)
+        self.visited.append(url)
+        data = request.urlopen(url).read().decode("utf-8")
+        raw_parent_urls = extract_course_index(data)
+        self.parent_urls = []
+        for link in raw_parent_urls:
+            if "@" in link: continue
+            if link == self.url: continue
+            if not link.startswith(self.url): continue
+            if link.endswith("/index-eng.html"): continue
+            
+
+            if re.search(r".*\?[^/]+",link): continue
+            if link.endswith("/index.html"):
+                link = link[:-10]
+            if link.find('http') >= 0:
+                if link not in self.urls:
+                    self.parent_urls.append(link)
+        
+
+semester_exam_index_remover_regex = re.compile(r"(?s)(?:.*?)/[vh]\d{2}/eksamen/", re.IGNORECASE)
+
 def check_url_and_update_storage(scraper,url, parent_url, parent_urls):
     """
     performs several checks on a given url and does one of three things:
@@ -47,7 +132,7 @@ def check_url_and_update_storage(scraper,url, parent_url, parent_urls):
         return
     
     url = relative_to_absolute_url(url, parent_url) # make sure url is full path and not relative
-    
+    if url.endswith(".tex"): return # ignore .tex files for now
     if url.find('http') >= 0:
         # storing and categorizing urls
         if url in scraper.visited: return
@@ -56,13 +141,16 @@ def check_url_and_update_storage(scraper,url, parent_url, parent_urls):
         if not "uio" in url: return
         if "@" in url: return
         if url in parent_urls: return
+        if semester_exam_index_remover_regex.match(url): return
+        if ".." in url: return
+        if scraper.subject_type not in url: return
         for disc in disqualifiers:
             if disc in url: return
         if url not in scraper.urls:
             scraper.urls.append(url)
             return url
 
-def reorder_urls_by_priority(urls, tolerance=10):
+def reorder_urls_by_priority(urls, tolerance=80):
     # tolerance is how many percent of the urls are to be returned
     global priorities
 
@@ -76,7 +164,7 @@ def reorder_urls_by_priority(urls, tolerance=10):
     for i,pri in enumerate(pri_vals_args):
         res.append(urls[pri])
     
-    return res[:int(len(urls)/tolerance)]
+    return res[:int(len(urls)*tolerance/100)]
 
 
 
@@ -99,23 +187,28 @@ def relative_to_absolute_url(link, parent_url):
             return parent_url+link
         else:
             return parent_url +"/" + link
-
+global REACHED_MAX_ALERED
+REACHED_MAX_ALERED = False
     
-def read_url(parent_url, scraper, queue):
-    global num_requests
-    if num_requests <= max_requests:
+def read_url(parent_url, queue):
+    global num_requests, REACHED_MAX_ALERED
+    if num_requests < max_requests:
+        num_requests += 1
+        if num_requests % 10 == 0:
+            print(f"Done {num_requests} requests")
         try:
-            num_requests += 1
-            print(num_requests)
+            
             data = request.urlopen(parent_url).read()
-        except Exception as e:#urllib.error.HTTPError:
-            print("ERROR", parent_url)
-            print(e)
-            print("\n")
+        except urllib.error.HTTPError:
+            print(f"Timed out: {parent_url}")
+            queue.put([])
             return
     else:
-        print("REACHED MAX NUMBER OF REQUESTS")
-        sys.exit(1)
+        if not REACHED_MAX_ALERED:
+            print("REACHED MAX NUMBER OF REQUESTS")
+            REACHED_MAX_ALERED = not REACHED_MAX_ALERED
+        queue.put([])
+        return
     
    
     data = [parent_url]+extract(data, parent_url)
@@ -125,120 +218,6 @@ def read_url(parent_url, scraper, queue):
 
 
 
-class LinkScrape:
-    urls = []
-    parent_urls = []
-    visited = []
-    pdfs = {}
-    valid_pdfs = []
-    potential_pdfs = []
-
-    max_depth = 1
-    subject_subfaculty_dict = {"fys":"fys", "fys-mek":"fys", "in":"ifi", "mat":"math", "mek":"math", "ast":"astro"}
-    def __init__(self, subject_code):
-        global ignore_urls
-        
-
-        self.base_url = "https://www.uio.no/studier/emner/matnat/"
-        self.subject_code = subject_code.upper()
-        subject_regex = re.search(r"([a-zA-Z\-]+)(\d+)", self.subject_code)
-        if subject_regex:
-            self.subject_type = subject_regex.group(1)
-            self.subject_uuid = subject_regex.group(2)
-        else:
-            print(f"Error, subject code '{subject_code}' is note valid")
-            sys.exit(1)
-        try:
-            self.subfaculty = self.subject_subfaculty_dict[self.subject_type.lower()]
-        except KeyError:
-            print(f"Subject '{subject_code}' of type '{self.subject_type}' not yet supported")
-            sys.exit(1)
-        #ignore_urls = [f'https://www.uio.no/studier/', f'https://www.uio.no/studier/emner/', f'https://www.uio.no/studier/emner/matnat/', 'https://www.uio.no/studier/emner/matnat/','https://www.uio.no/studier/emner/matnat/fys/',f'https://www.uio.no/studier/emner/matnat/fys/{self.subject_code}']
-        
-    
-    def start(self, depth=0,**kwargs):
-        self.start_index_scraper()
-        self.urls_to_be_checked = self.parent_urls.copy()  # urls_to_be_checked gets dynamically updated
-        try:
-            for i in range(self.max_depth):
-                
-                child_urls_ = fetch_parallel(self, self.urls_to_be_checked)#, iteration=i)
-                child_urls = [foo[0][1:] for foo in child_urls_]
-                #child_urls = list(itertools.chain.from_iterable(child_urls))
-                
-                if i == 0:
-                    parent_urls = [foo[0][0] for foo in child_urls_]
-                new_urls_to_be_checked = []
-                for childs in child_urls:
-                    for url in childs[1:]:
-                        if scrape_result:=check_url_and_update_storage(self, url, childs[0], parent_urls):
-                            new_urls_to_be_checked.append(scrape_result)
-                        
-                
-                #print("\n\n\n")
-                
-                #print("\n".join([f"{name}: {link}" for name,link in self.pdfs.items()]))
-                self.urls_to_be_checked = reorder_urls_by_priority(new_urls_to_be_checked.copy())
-                print("\n".join(self.urls_to_be_checked))
-                
-                #child_urls = [relative_to_absolute_url(foo) for foo in child_urls]
-                #print(child_urls)
-                #self.urls_to_be_checked = child_urls.copy()
-            # for parent_url in self.parent_urls:
-                
-            #     #print(f"\nchecking {index_url} ... \n")
-            #     try:
-            #         self.child_scraping(parent_url)
-            #     except error.HTTPError:
-            #         pass
-            #     #print(f"{j+1}/{len(self.parent_urls)}")
-        except KeyboardInterrupt:
-            pass
-
-        
-
-    
-
-    def start_index_scraper(self):
-
-        url = f"https://www.uio.no/studier/emner/matnat/{self.subfaculty}/{self.subject_code}/"
-        self.url = url
-        self.parent_urls.append(url)
-        self.visited.append(url)
-        #print(url)
-        data = request.urlopen(url).read().decode("utf-8")
-        raw_parent_urls = extract_course_index(data)
-        self.parent_urls = []
-        for link in raw_parent_urls:
-            if "@" in link: continue
-            if link == self.url: continue
-            if not link.startswith(self.url): continue
-            if link.endswith("/index-eng.html"): continue
-            
-
-            if re.search(r".*\?[^/]+",link): continue
-            if link.endswith("/index.html"):
-                link = link[:-10]
-            if link.find('http') >= 0:
-                if link not in self.urls:
-                    self.parent_urls.append(link)
-        
-
-        #print("\n".join(self.parent_urls))
-    
-    def child_scraping(self, child_urls):
-        self.visited += child_urls #append(url)
-        result = Queue()
-        res = []
-
-        threads = [threading.Thread(target=check_url_and_update_storage, args = (self, url, result)) for url in child_urls]
-        for t in threads:
-            t.start()
-        for t in threads:
-            res.append(extract(result.get().decode("latin-1")))
-        for t in threads:
-            t.join()
-        return res
 
 
 
@@ -250,7 +229,6 @@ course_main_body_regex = re.compile(r"(?s)<!--startindex-->(.*)<!--stopindex-->"
 course_messages_regex = re.compile(r"(?s)(<div class=\"vrtx-messages-header\">(?:.*?)</div>)(.*?)(<div class=\"vrtx-messages\">(?:.*?)</div>)", re.IGNORECASE)
 course_index_semester_list_regex = re.compile(r"(?s)(<div class=\"vrtx-frontpage-box grey-box\" id=\"vrtx-course-semesters\">(?:.*?)</div>)", re.IGNORECASE)
 
-#HTML_TAG_REGEX = re.compile(r'<ahref=\"([^\"^#^@^\?]*)\"[^>]*>([^<]*)<\/a>', re.IGNORECASE)
 extract_href_regex = re.compile(r'(?s)href=\"([^\"^#^@^\?\~]*)\"[^>]*>(?:.*?)</a>', re.IGNORECASE)
 
 def extract_course_index(content):
@@ -320,12 +298,12 @@ def merge(master, rel):
 
 
 
-def fetch_parallel(scraper, urls_to_load):
+def fetch_parallel(urls_to_load):
     result = Queue()
     res = []
 
 
-    threads = [threading.Thread(target=read_url, args = (url, scraper, result)) for url in urls_to_load]
+    threads = [threading.Thread(target=read_url, args = (url, result)) for url in urls_to_load]
     for t in threads:
         t.start()
     for t in threads:
@@ -334,11 +312,29 @@ def fetch_parallel(scraper, urls_to_load):
         t.join()
     return res
 
-if __name__ == '__main__':
-    scraper = LinkScrape(sys.argv[1])
-    scraper.start()
 
+parser = argparse.ArgumentParser(description='Scrape all semester pages of a UiO subject in order to get the urls of pdfs of old exams and their solutions.\n Made by Bror Hjemgaard, 2021')
+parser.add_argument('SUBJECT', metavar='SUBJECT', nargs=1,
+                    help='Subject code of a matnat subject. Case insensitive')
+parser.add_argument('-d', dest='depth', metavar="depth", default=2,
+                    help='How many layers deep the scraping should go. This caues the number of requests to grow exponentially. Default: 2')
+parser.add_argument("-r", dest="requests",  metavar="requests", default=60, help="max. number of requests to make. Increase at own risk. Default: 60")
+
+
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    max_requests = int(args.requests)
+    max_depth = int(args.depth)
+    subject = args.SUBJECT[0]
+    start = time.time()
+    #sys.exit()
+    scraper = LinkScrape(subject = subject, max_depth=max_depth, max_requests = max_requests)
+    scraper.start()
+    end = time.time()
     print()
+    print(f"Found {len(scraper.pdfs.keys())} items in {round(end-start,2)}s after {num_requests} requests")
     print("===RESULTS===")
     print("\n".join([f"{name}: {link}" for name,link in scraper.pdfs.items()]))
 

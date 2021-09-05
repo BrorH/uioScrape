85c8eedccfd32594131cfb119654bda24766dbca
Bror <bahjemga@uio.no>
2021-09-05 18:52:03 +0200

    added windows support

--- scraper.py
+++ scraper.py
@@ -10,23 +10,33 @@ from bs4 import BeautifulSoup
 import json
 import os
 from pathlib import Path
-
+from sys import platform
 
 unique_pdfs = []
 download_size = 0
 request_count = 0
 download_count = 0
-
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    if platform == "win32":
+        HEADER = ''
+        OKBLUE = ''
+        OKCYAN = ''
+        OKGREEN = ''
+        WARNING = ''
+        FAIL = ''
+        ENDC = ''
+        BOLD = ''
+        UNDERLINE = ''
+    else:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
 
 
 def bytesize_to_readable(bytes, failsafe =True):
